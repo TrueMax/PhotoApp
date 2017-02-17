c14e8613e3974ff0001dc3b4faf9a5ee75ac510c
Maxim <maximnatural@gmail.com>
2017-02-17 21:44:01 +0300

    Initial Commit

---
+++ photoapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoapp
+//
+//  Created by Maxim on 17.02.17.
+//  Copyright © 2017 Maxim Abakumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
